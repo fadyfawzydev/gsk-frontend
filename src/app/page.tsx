36585a51533ce0bae861ff8ed9c868cd3e471386
fadyfawzydev <fady.fawzy@theaddressholding.com>
2024-03-13 01:59:07 +0200

    fix responsive issue

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -22,21 +22,14 @@ export default function Home() {
         <div className="flex h-full w-full">
           <div className="w-4/12 h-full">
             <div className="flex flex-col h-full w-full relative justify-between">
-              <div className="w-full h-[75%] rounded-bl-[32px] rounded-br-[32px] shadow-inner backdrop-blur-[47px] flex flex-col justify-center items-center gap-10 relative !m-0">
+              <div className="w-full h-[75%] 2xl:h-[75%] rounded-bl-[32px] rounded-br-[32px] shadow-inner backdrop-blur-[47px] flex flex-col justify-center items-center gap-10 relative !m-0">
                 <Image
                   src={"/logos/scan_to_play.webp"}
                   height={37}
                   width={238}
                   alt="Scan to Play"
                 />
-                <div
-                  style={{
-                    height: "auto",
-                    margin: "0 auto",
-                    width: "100%",
-                  }}
-                  className="max-w-[50%]"
-                >
+                <div className="min-w-[75%] 2xl:min-w-[85%]  mx-auto h-auto mb-5">
                   <QRCode
                     size={256}
                     style={{ height: "auto", maxWidth: "100%", width: "100%" }}
@@ -45,16 +38,15 @@ export default function Home() {
                     viewBox={`0 0 256 256`}
                   />
                 </div>
-                <div className="absolute -bottom-[50px] w-[90%] h-[102px] px-[18px] py-8 bg-white bg-opacity-60 rounded-[32px] shadow-inner backdrop-blur-[47px] justify-center items-center gap-3.5 inline-flex">
-                  <div className="text-center text-orange-950 lg:text-3xl 2xl:text-4xl font-semibold font-['Metropolis'] flex items-center gap-4">
+                <button className="absolute -bottom-[50px] w-[90%] h-[102px] px-[18px] py-8 bg-white bg-opacity-60 rounded-[32px] shadow-inner backdrop-blur-[47px] justify-center items-center gap-3.5 inline-flex">
+                  <span className="text-center text-orange-950 lg:text-2xl 2xl:text-3xl font-semibold font-['Metropolis'] flex items-center gap-4">
                     <PuzzleIcon /> Start Playing Now
-                  </div>
-                </div>
+                  </span>
+                </button>
               </div>
               <div className="relative w-full h-[25%] flex flex-col items-center justify-end ">
                 <ArrowLine
-                  className="absolute right-0 bottom-[55px] z-10"
-                  width={40}
+                  className="absolute right-[21px] bottom-[70px] 3xl:bottom-[102px] z-10 w-[40px] 2xl:w-[45px] "
                 />
                 <div className="bg-white bg-opacity-50 rounded-[32px] shadow-inner backdrop-blur-[47px] flex items-center justify-center w-full gap-4 mb-4 min-h-[110px]">
                   <span className="text-orange-950 lg:text-3xl 2xl:text-4xl font-bold font-['Metropolis']">
